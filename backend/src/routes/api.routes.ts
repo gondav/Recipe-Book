eb8758cf3e85be6895c684bad4493bb2aa4bb50e
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-07-19 20:21:20 +0200

    [2] add recipeRouter middleware

--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import { recipeRouter } from './recipe.routes';
 
 const apiRouter = express.Router();
+
 apiRouter.use(cors());
 apiRouter.use(express.json());
+apiRouter.use('/recipe', recipeRouter);
 
 export default apiRouter;
