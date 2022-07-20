0ed98a4d98b377846c22545f45b578f92df1ff0f
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-07-20 20:15:20 +0200

    [3] add morgan middleware with 'dev' option

--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import { recipeRouter } from './recipe.routes';
 
 const apiRouter = express.Router();
 
+apiRouter.use(morgan('dev'));
 apiRouter.use(cors());
 apiRouter.use(express.json());
+
 apiRouter.use('/recipe', recipeRouter);
 
 export default apiRouter;
