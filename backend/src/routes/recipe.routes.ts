860ffa6b231f110b823823d0e1dc8348770ea68d
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-07-20 20:11:52 +0200

    [3] add errorChecker to /recipes endpoint

--- backend/src/routes/recipe.routes.ts
+++ backend/src/routes/recipe.routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
+import errorChecker from '../middlewares/errorChecker/errorChecker';
 import { recipeController } from '../controllers/recipeController';
 
 export const recipeRouter = express.Router();
 
-recipeRouter.get('/recipes', recipeController.getRecipes);
+recipeRouter.get('/recipes', errorChecker(recipeController.getRecipes));
