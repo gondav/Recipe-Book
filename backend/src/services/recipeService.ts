01d054e75c706859e49224989b0c5c583d0f26eb
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-07-21 22:46:20 +0200

    [4] add getRecipe function to recipeService

--- backend/src/services/recipeService.ts
+++ backend/src/services/recipeService.ts
@@ -1,12 +1,18 @@
 import { IRecipeDomainModel } from '../models/domainModels/IRecipeDomainModel';
 import { recipeRepository } from '../repositories/recipeRepository';
+import { notFoundError } from './errorCreatorService';
 
 export const recipeService = {
-  async getRecipes(): Promise<IRecipeDomainModel[] | Error> {
-    try {
-      return await recipeRepository.getRecipes();
-    } catch (error) {
-      return Promise.reject(error);
+  async getRecipes(): Promise<IRecipeDomainModel[]> {
+    return await recipeRepository.getRecipes();
+  },
+
+  async getRecipe(recipe_id: number): Promise<IRecipeDomainModel> {
+    const recipe = await recipeRepository.getRecipe(recipe_id);
+
+    if (!recipe.recipe_id) {
+      return Promise.reject(notFoundError('Ticket was not found'));
     }
+    return recipe;
   }
 };
