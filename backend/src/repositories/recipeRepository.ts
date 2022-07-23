717c58b9c23003a427ad0154d9f93ddb71539f57
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-07-23 13:40:24 +0200

    [7] correction: changed snake case to camel case and added return values to queries

--- backend/src/repositories/recipeRepository.ts
+++ backend/src/repositories/recipeRepository.ts
@@ -4,29 +4,32 @@ import { db } from '../data/connection';
 
 export const recipeRepository = {
   getRecipes(): Promise<IRecipeDomainModel[]> {
-    return db.query(
+    return db.query<IRecipeDomainModel[]>(
       'SELECT recipe.recipe_id, recipe.recipe_name, description, image_id FROM recipe',
       []
     );
   },
 
-  getRecipe(recipe_id: number): Promise<IRecipeDomainModel> {
-    return db.query('SELECT * FROM recipe WHERE recipe_id = ?', [
-      String(recipe_id)
-    ]);
+  getRecipe(recipeId: number): Promise<IRecipeDomainModel> {
+    return db.query<IRecipeDomainModel>(
+      'SELECT * FROM recipe WHERE recipe_id = ?',
+      [String(recipeId)]
+    );
   },
 
   getRecipeIngredients(
-    recipe_id: number
+    recipeId: number
   ): Promise<IRecipeIngredientsDomainModel[]> {
     const recipeDetails =
       'SELECT qty_amount, measurement_description, ingredient_name FROM recipe JOIN recipe_details ON recipe.recipe_id = recipe_details.recipe_id JOIN measurement_unit ON recipe_details.measurement_id = measurement_unit.measurement_id JOIN ingredient ON recipe_details.ingredient_id = ingredient.ingredient_id JOIN measurement_qty ON recipe_details.measurement_qty_id = measurement_qty.measurement_qty_id WHERE recipe.recipe_id = ?';
-    return db.query(recipeDetails, [String(recipe_id)]);
+    return db.query<IRecipeIngredientsDomainModel[]>(recipeDetails, [
+      String(recipeId)
+    ]);
   },
 
-  getRecipesByTagName(tag_name: string): Promise<IRecipeDomainModel[]> {
+  getRecipesByTagName(tagName: string): Promise<IRecipeDomainModel[]> {
     const recipes =
       'SELECT DISTINCT recipe.recipe_id, recipe_name, description, image_id FROM recipe JOIN recipe_details ON recipe.recipe_id = recipe_details.recipe_id JOIN recipe_tag ON recipe_details.recipe_tag_id = recipe_tag.tag_id WHERE tag = ?';
-    return db.query(recipes, [tag_name]);
+    return db.query<IRecipeDomainModel[]>(recipes, [tagName]);
   }
 };
