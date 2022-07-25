88ecafc15133bd0cf92e9065bba892b4c6a11782
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-07-25 11:59:01 +0200

    [9] add notAcceptableError

--- backend/src/services/errorCreatorService.ts
+++ backend/src/services/errorCreatorService.ts
@@ -12,5 +12,6 @@ const generalError =
 export const badRequestError = generalError(400);
 export const forbiddenError = generalError(403);
 export const notFoundError = generalError(404);
+export const notAcceptableError = generalError(406);
 export const conflictError = generalError(409);
 export const serverError = generalError(500);
