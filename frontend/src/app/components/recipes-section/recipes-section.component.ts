86d655f4b1cc98ec80721637b6a55fe965ebcd79
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-07 16:29:36 +0200

    [15] add component

---
+++ frontend/src/app/components/recipes-section/recipes-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-section',
+  templateUrl: './recipes-section.component.html',
+  styleUrls: ['./recipes-section.component.scss']
+})
+export class RecipesSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
