4a78fc68eaf4917f1bcb27044916d9f1ea3f6f30
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-08 14:11:33 +0200

    [17] add recipe input

--- frontend/src/app/components/recipe-card/recipe-card.component.ts
+++ frontend/src/app/components/recipe-card/recipe-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IRecipeViewModel } from '../../shared/models/viewmodels/IRecipeViewModel';
 
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
-  styleUrls: ['./recipe-card.component.scss']
+  styleUrls: ['./recipe-card.component.scss'],
 })
 export class RecipeCardComponent implements OnInit {
+  @Input() recipe: IRecipeViewModel;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
