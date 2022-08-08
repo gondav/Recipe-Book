919b51722d9b760ec41034b26116ec5d02166b71
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-08 14:13:00 +0200

    [17] import landing page component and background image url pipe

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,9 +8,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { RecipeCardComponent } from './components/recipe-card/recipe-card.component';
 import { RecipesSectionComponent } from './components/recipes-section/recipes-section.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { BackgroundImgUrlPipe } from './pipes/background-img-url.pipe';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipeCardComponent, RecipesSectionComponent],
+  declarations: [AppComponent, HeaderComponent, RecipeCardComponent, RecipesSectionComponent, LandingPageComponent, BackgroundImgUrlPipe],
   imports: [
     BrowserModule,
     AppRoutingModule,
