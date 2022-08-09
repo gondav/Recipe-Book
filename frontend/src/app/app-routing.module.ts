5db170b0471401784123f2573f48e6a0f739df6e
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-09 21:46:55 +0200

    [18] add basic routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationPageComponent } from './components/authentication/authentication-page/authentication-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  { path: 'recipes', component: LandingPageComponent },
+  { path: 'login', component: AuthenticationPageComponent },
+  { path: 'register', component: AuthenticationPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
