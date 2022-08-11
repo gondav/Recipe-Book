3f89e3f9726f8eb9a6057397355277cd39997b99
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-11 16:04:12 +0200

    [18] add login form component

---
+++ frontend/src/app/components/authentication/login-form/login-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss'],
+})
+export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  submitLogin(): void {}
+}
