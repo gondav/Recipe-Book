2fb191ef65ee4baa7e66e73b0dec15360e7461f1
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-12 14:05:27 +0200

    [19] register form component and new authentication page comp. import added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthenticationPageComponent } from './components/authentication/authentication-page/authentication-page.component';
+import { AuthenticationPageComponent } from './components/authentication/authentication-page.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 const routes: Routes = [
