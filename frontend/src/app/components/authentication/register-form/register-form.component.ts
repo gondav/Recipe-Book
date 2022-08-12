cba5f68c450558c2554fcba4dabc0a6cbf01c0d6
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-12 14:07:36 +0200

    [19] add register form component

---
+++ frontend/src/app/components/authentication/register-form/register-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.scss'],
+})
+export class RegisterFormComponent implements OnInit {
+  registerForm: FormGroup;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(/^(?=.*\d)(?=.*[a-z]).{6,}$/),
+      ]),
+    });
+  }
+
+  submitRegister(): void {}
+}
