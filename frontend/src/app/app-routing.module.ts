0b6e0fd95f91d66f70f831ce079b7d4b1903bf56
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-16 14:43:46 +0200

    [24] add saved recipes path

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationPageComponent } from './components/authentication/authentication-page.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { SavedRecipesComponent } from './components/saved-recipes/saved-recipes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
   { path: 'recipes', component: LandingPageComponent },
+  { path: 'saved-recipes', component: SavedRecipesComponent },
   { path: 'login', component: AuthenticationPageComponent },
   { path: 'register', component: AuthenticationPageComponent },
 ];
