e259c7154eac4cafb8bfa255237373580687752e
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-16 16:03:35 +0200

    [24] get saved recipes and user id dynamically

--- frontend/src/app/components/saved-recipes/saved-recipes.component.ts
+++ frontend/src/app/components/saved-recipes/saved-recipes.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe-service/recipe.service';
+import { IRecipeViewModel } from '../../shared/models/viewmodels/IRecipeViewModel';
 
 @Component({
   selector: 'app-saved-recipes',
   templateUrl: './saved-recipes.component.html',
-  styleUrls: ['./saved-recipes.component.scss']
+  styleUrls: ['./saved-recipes.component.scss'],
 })
 export class SavedRecipesComponent implements OnInit {
+  recipes: IRecipeViewModel[] = [];
+  sectionTitle: string = 'SAVED RECIPES';
+  userId = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private recipeService: RecipeService) {
+    this.recipeService.getSavedRecipes().subscribe({
+      next: (recipes) => {
+        this.recipes = recipes;
+      },
+    });
   }
 
+  ngOnInit(): void {}
 }
