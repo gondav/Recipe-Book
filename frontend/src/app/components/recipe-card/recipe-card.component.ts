5c0dd4f75623830ce80e9da1dcea132582a39a8d
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-17 10:47:08 +0200

    [26] user now can add and remove recipes from favorites by clicking bookmark

--- frontend/src/app/components/recipe-card/recipe-card.component.ts
+++ frontend/src/app/components/recipe-card/recipe-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth-service/auth.service';
+import { RecipeService } from 'src/app/services/recipe-service/recipe.service';
 import { IRecipeViewModel } from '../../shared/models/viewmodels/IRecipeViewModel';
 
 @Component({
@@ -8,8 +10,52 @@ import { IRecipeViewModel } from '../../shared/models/viewmodels/IRecipeViewMode
 })
 export class RecipeCardComponent implements OnInit {
   @Input() recipe: IRecipeViewModel;
+  isRecipeFavorite = false;
 
-  constructor() {}
+  constructor(
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.setIsRecipeFavorite();
+  }
+
+  setIsRecipeFavorite(): void {
+    const favoriteRecipes =
+      this.recipeService.getFavoriteRecipesFromLocalStorage();
+
+    if (favoriteRecipes.includes(this.recipe.recipeId)) {
+      this.isRecipeFavorite = true;
+    }
+  }
+
+  toggleFavoriteRecipes() {
+    const userId = this.authService.getUserId();
+    const recipeId = this.recipe.recipeId;
+
+    this.isRecipeFavorite
+      ? this.removeRecipeFromFavorites(recipeId)
+      : this.addRecipeToFavorites(userId, recipeId);
+  }
+
+  addRecipeToFavorites(userId: number, recipeId: number): void {
+    this.recipeService.addFavoriteRecipe({ userId, recipeId }).subscribe({
+      next: (_response) => {
+        this.recipeService.addFavoriteRecipeToLocalStorage(recipeId);
+        this.isRecipeFavorite = true;
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  removeRecipeFromFavorites(recipeId: number): void {
+    this.recipeService.removeRecipeFromFavorites(recipeId).subscribe({
+      next: (_response) => {
+        this.isRecipeFavorite = false;
+        this.recipeService.removeFavoriteRecipeFromLocalStorage(recipeId);
+      },
+      error: (error) => console.log(error),
+    });
+  }
 }
