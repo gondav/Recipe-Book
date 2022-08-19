50249b94fcc1ec02330b80fcc9304d7c5d067736
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-19 22:52:22 +0200

    [27] implemented lazy loading on /saved-recipes path

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationPageComponent } from './features/authentication/authentication-page.component';
 import { LandingPageComponent } from './features/landing-page/landing-page.component';
-import { SavedRecipesComponent } from './features/saved-recipes/saved-recipes.component';
-import { AuthGuard } from './core/services/authGuard/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
   { path: 'recipes', component: LandingPageComponent },
   {
     path: 'saved-recipes',
-    component: SavedRecipesComponent,
-    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./features/saved-recipes/saved-recipes.module').then(
+        (m) => m.SavedRecipesModule
+      ),
   },
   { path: 'login', component: AuthenticationPageComponent },
   { path: 'register', component: AuthenticationPageComponent },
