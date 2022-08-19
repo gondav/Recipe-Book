c3562e22caaf7336a8bc58aedbf974491c038f87
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-19 17:32:12 +0200

    [27] changed import paths to the correct one

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthenticationPageComponent } from './components/authentication/authentication-page.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { SavedRecipesComponent } from './components/saved-recipes/saved-recipes.component';
+import { AuthenticationPageComponent } from './features/authentication/authentication-page.component';
+import { LandingPageComponent } from './features/landing-page/landing-page.component';
+import { SavedRecipesComponent } from './features/saved-recipes/saved-recipes.component';
 import { AuthGuard } from './core/services/authGuard/auth.guard';
 
 const routes: Routes = [
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,14 +8,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { RecipeCardComponent } from './shared/components/recipes-section/recipe-card/recipe-card.component';
 import { RecipesSectionComponent } from './shared/components/recipes-section/recipes-section.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { LoginFormComponent } from './components/authentication/login-form/login-form.component';
+import { LandingPageComponent } from './features/landing-page/landing-page.component';
+import { LoginFormComponent } from './features/authentication/login-form/login-form.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BackgroundImgUrlPipe } from './pipes/background-img-url.pipe';
-import { RegisterFormComponent } from './components/authentication/register-form/register-form.component';
-import { AuthenticationPageComponent } from './components/authentication/authentication-page.component';
+import { RegisterFormComponent } from './features/authentication/register-form/register-form.component';
+import { AuthenticationPageComponent } from './features/authentication/authentication-page.component';
 import { TokenInterceptor } from '../app/core/interceptors/tokenInterceptor/token.interceptor';
-import { SavedRecipesComponent } from './components/saved-recipes/saved-recipes.component';
+import { SavedRecipesComponent } from './features/saved-recipes/saved-recipes.component';
 
 @NgModule({
   declarations: [
