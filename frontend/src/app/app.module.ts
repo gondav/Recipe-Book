db338e5ba8783d99c0e2726be214bdc8e1fc0c24
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-20 20:40:32 +0200

    [27] moved auth page comp, login form comp and register form comp to authentication module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,32 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { LoginFormComponent } from './features/authentication/login-form/login-form.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RegisterFormComponent } from './features/authentication/register-form/register-form.component';
-import { AuthenticationPageComponent } from './features/authentication/authentication-page.component';
 import { TokenInterceptor } from '../app/core/interceptors/tokenInterceptor/token.interceptor';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthenticationPageComponent,
-    LoginFormComponent,
-    RegisterFormComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
     SharedModule,
-    ReactiveFormsModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
--- frontend/src/app/features/authentication/authentication.module.ts
+++ frontend/src/app/features/authentication/authentication.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
+import { AuthenticationPageComponent } from './authentication-page.component';
+import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, SharedModule],
+  declarations: [
+    AuthenticationPageComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    AuthenticationRoutingModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AuthenticationModule {}
