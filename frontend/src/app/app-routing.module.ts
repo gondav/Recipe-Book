0212f6c01e18a3dd8dd844391b58a0ac2c49404a
Viktoria Gonda <viktoria.gonda@gmail.com>
2022-08-25 15:46:59 +0200

    changed folder paths

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,28 +6,28 @@ const routes: Routes = [
   {
     path: 'recipes',
     loadChildren: () =>
-      import('./features/landing-page/landing-page.module').then(
+      import('../app/features/landing-page/landing-page.module').then(
         (m) => m.LandingPageModule
       ),
   },
   {
     path: 'saved-recipes',
     loadChildren: () =>
-      import('./features/saved-recipes/saved-recipes.module').then(
+      import('../app/features/saved-recipes/saved-recipes.module').then(
         (m) => m.SavedRecipesModule
       ),
   },
   {
     path: 'login',
     loadChildren: () =>
-      import('./features/authentication/authentication.module').then(
+      import('../app/features/authentication/authentication.module').then(
         (m) => m.AuthenticationModule
       ),
   },
   {
     path: 'register',
     loadChildren: () =>
-      import('./features/authentication/authentication.module').then(
+      import('../app/features/authentication/authentication.module').then(
         (m) => m.AuthenticationModule
       ),
   },
